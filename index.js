1c08ad2ca9e0e335c5bfec8a4b2239cedc7d7da8
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-05-04 11:16:41 +0300

    I used axios and async await this time, its more readable and easy to understand from others

--- index.js
+++ index.js
@@ -1,180 +1,181 @@
-import fetch from "node-fetch";
-import connection from './db.js'
-import { config } from "dotenv";
-config();
+import axios from "axios";
+import connection from "./db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Parse command line arguments
-const githubUsername = process.argv[2];
-const freshdeskSubdomain = process.argv[3];
+const { FRESHDESK_API_KEY, GITHUB_TOKEN, FRESHDESK_SUBDOMAIN } = process.env;
 
-if (!githubUsername || !freshdeskSubdomain) {
-  console.error("Please provide a GitHub username and a Freshdesk subdomain.");
-  process.exit(1);
-}
+const githubApi = axios.create({
+  baseURL: "https://api.github.com",
+  headers: {
+    Authorization: `token ${GITHUB_TOKEN}`,
+  },
+});
 
-// Set up API endpoints and authentication
-const githubUrl = `https://api.github.com/users/${githubUsername}`;
-console.log(githubUrl);
-const freshdeskContactsUrl = `https://${freshdeskSubdomain}.freshdesk.com/api/v2/contacts`;
-const freshdeskAuth = `${process.env.FRESHDESK_API_KEY}:X`;
+const freshdeskApi = axios.create({
+  baseURL: `https://${FRESHDESK_SUBDOMAIN}.freshdesk.com/api/v2`,
+  headers: {
+    Authorization: `Basic ${Buffer.from(`${FRESHDESK_API_KEY}:X`).toString(
+      "base64"
+    )}`,
+  },
+});
 
-// Retrieve user data from GitHub API
-export const getGitHubUserData = async () => {
-  const response = await fetch(githubUrl, {
-    headers: {
-      Authorization: `token ${process.env.GITHUB_TOKEN}`,
-    },
-  });
+const getUserData = async (username) => {
+  try {
+    const { data } = await githubApi.get(`/users/${username}`);
+    const email =
+      data.email ||
+      (await githubApi.get(data.url).then((response) => response.data.email));
 
-  if (!response.ok) {
-    throw new Error(`Failed to fetch GitHub user data: ${response.statusText}`);
+    console.log("Retrieved email:", email);
+
+    return { ...data, email };
+  } catch (error) {
+    throw new Error(`Failed to fetch GitHub user data: ${error.message}`);
   }
+};
 
-  const data = await response.json();
+export { getUserData };
 
-  // Try to get email from user data or public profile
-  const email =
-    data.email ||
-    (await fetch(`${data.url}.json`, {
-      headers: {
-        Authorization: `token ${process.env.GITHUB_TOKEN}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => data.email));
-  console.log("Retrieved email:", email);
-
-  // Insert user data into the MySQL database
-  const user = {
-    login: data.login,
-    name: data.name,
-    created_at: new Date(data.created_at),
+const createContact = async (userData) => {
+  const data = {
+    name: userData.name || userData.login,
+    email: userData.email,
   };
 
-  connection.query("INSERT INTO users SET ?", user, (err, result) => {
-    if (err) {
-      console.error("Error inserting user:", err);
-    } else {
-      console.log("User inserted successfully with ID:", result.insertId);
+  const created_at = new Date(userData.created_at)
+    .toISOString()
+    .replace("T", " ")
+    .replace("Z", "");
+
+  try {
+    const { data: contact } = await freshdeskApi.post("/contacts", data);
+    console.log(`Contact ${contact.id} created successfully in Freshdesk.`);
+    // insert new contact info into database
+    connection.query(
+      "INSERT INTO users (id, login, name, email, created_at, freshdesk_contact_id) VALUES (?, ?, ?, ?, ?, ?)",
+      [
+        userData.id,
+        userData.login,
+        userData.name,
+        userData.email,
+        created_at,
+        contact.id,
+      ],
+      (error, results) => {
+        if (error) {
+          console.error("Error inserting new user:", error);
+        }
+      }
+    );
+    return contact;
+  } catch (error) {
+    if (error.response && error.response.status === 409) {
+      console.log(`Contact already exists in Freshdesk, updating...`);
+      const existingContact = await findContactByEmail(userData.email);
+      return updateContact(existingContact.id, userData);
     }
-  });
-
-  // Return user data with updated email field
-  return { ...data, email };
-};
-
-// Find an existing contact in Freshdesk by email address
-export const findExistingContact = async (email) => {
-  const url = `${freshdeskContactsUrl}?email=${email}`;
-  console.log(url);
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `Basic ${Buffer.from(freshdeskAuth).toString("base64")}`,
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `Failed to search for contact in Freshdesk: ${response.statusText}`
-    );
+    throw new Error(`Failed to create Freshdesk contact: ${error.message}`);
   }
-
-  const results = await response.json();
-  return results.length > 0 ? results[0] : null;
 };
 
-export const createFreshdeskContact = async (githubUserData) => {
+export { createContact };
+
+const updateContact = async (contactId, userData) => {
   const data = {
-    name: githubUserData.name || githubUserData.login,
-    email: githubUserData.email,
+    name: userData.name || userData.login,
+    email: userData.email,
   };
 
-  const authHeader = `Basic ${Buffer.from(freshdeskAuth).toString("base64")}`;
-  const response = await fetch(freshdeskContactsUrl, {
-    method: "POST",
-    headers: {
-      Authorization: authHeader,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `Failed to create Freshdesk contact: ${response.statusText}`
+  try {
+    const { data: contact } = await freshdeskApi.put(
+      `/contacts/${contactId}`,
+      data
     );
-  }
+    console.log(`Contact ${contact.id} updated successfully in Freshdesk.`);
 
-  return await response.json();
+    return contact;
+  } catch (error) {
+    throw new Error(`Failed to update Freshdesk contact: ${error.message}`);
+  }
 };
 
-// Update an existing contact in Freshdesk with new data
-export const updateFreshdeskContact = async (contactId, githubUserData) => {
-  const data = {
-    name: githubUserData.name || githubUserData.login,
-    email: githubUserData.email,
-  };
+export { updateContact };
 
-  const url = `${freshdeskContactsUrl}/${contactId}`;
-  const response = await fetch(url, {
-    method: "PUT",
-    headers: {
-      Authorization: `Basic ${Buffer.from(freshdeskAuth).toString("base64")}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+const findContactByEmail = async (email) => {
+  try {
+    const { data: contacts } = await freshdeskApi.get(
+      `/contacts?email=${email}`
+    );
 
-  if (!response.ok) {
+    return contacts.length > 0 ? contacts[0] : null;
+  } catch (error) {
     throw new Error(
-      `Failed to update Freshdesk contact: ${response.statusText}`
+      `Failed to search for contact in Freshdesk: ${error.message}`
     );
   }
-
-  return await response.json();
 };
 
-// Main function
+export { findContactByEmail };
+
 const main = async () => {
   try {
-    // Retrieve user data from GitHub API
-    const githubUserData = await getGitHubUserData();
+    const [username, subdomain] = process.argv.slice(2);
 
-    // Find an existing contact in Freshdesk by email address
-    const existingContact = await findExistingContact(githubUserData.email);
+    if (!username || !subdomain) {
+      throw new Error(
+        "Please provide a GitHub username and a Freshdesk subdomain."
+      );
+    }
+
+    // get user data from GitHub
+    const userData = await getUserData(username);
+
+    // find existing contact by email
+    const existingContact = await findContactByEmail(userData.email);
 
     if (existingContact) {
-      // Update existing contact with new data
-      console.log(
-        `Updating existing contact ${existingContact.id} in Freshdesk...`
-      );
-      await updateFreshdeskContact(existingContact.id, githubUserData);
-      console.log(`Contact ${existingContact.id} updated successfully.`);
+      // update existing contact with user data
+      const updatedContact = await updateContact(existingContact.id, userData);
 
-      // Update the MySQL database with the contact's ID
+      // update user record with freshdesk contact id
       connection.query(
         "UPDATE users SET freshdesk_contact_id = ? WHERE login = ?",
-        [existingContact.id, githubUserData.login],
-        (err, result) => {
-          if (err) {
-            console.error("Error updating user:", err);
+        [updatedContact.id, username],
+        (error, results) => {
+          if (error) {
+            console.error("Error updating user:", error);
           } else {
-            console.log("User updated successfully.");
+            console.log(
+              `User ${username} updated successfully with contact ID ${updatedContact.id}.`
+            );
           }
         }
       );
     } else {
-      // Create new contact in Freshdesk
-      console.log(`Creating new contact in Freshdesk...`);
-      const newContact = await createFreshdeskContact(githubUserData);
-      console.log(`Contact ${newContact.id} created successfully.`);
-    }
+      // create new contact with user data
+      const newContact = await createContact(userData);
 
-    process.exit(0);
+      // update user record with freshdesk contact id
+      connection.query(
+        "UPDATE users SET freshdesk_contact_id = ? WHERE login = ?",
+        [newContact.id, username],
+        (error, results) => {
+          if (error) {
+            console.error("Error updating user:", error);
+          } else {
+            console.log(
+              `User ${username} created successfully with contact ID ${newContact.id}.`
+            );
+          }
+        }
+      );
+    }
   } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.error("Error:", error.message);
+  } finally {
+    connection.end();
   }
 };
-
 main();
